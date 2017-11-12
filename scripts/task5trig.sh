163e4d39021cec073d261b9cbfb2201a5d1885d5
piekvossen <piek.vossen@vu.nl>
2017-11-12 20:48:00 +0100

    -- first  implementation for the task: we have results.... -- events from the same file are merged: one event per document heuristic -- events across documents are merged when nr of hasActor objects match above threshold -- run parameters added -- @TODO is nr of killings and injured for subtask 3 -- @TODO increase constraint matching -- @TODO KS integration -- @TODO lots of clean up and documentation

---
+++ scripts/task5trig.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+#!/usr/bin/env bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ROOT="$( cd $DIR && cd .. && pwd)"
+LIB="$ROOT"/lib
+QUESTIONPATH=$1
+TRIGPATH=$2
+CONLLPATH=$3
+THRESHOLD=S4
+
+java -Xmx2000m -cp "$LIB/nwr-semeval2018-5-1.0-SNAPSHOT-jar-with-dependencies.jar" Task5Trig --question $QUESTIONPATH -trig-files TRIGPATH --conll-files $CONLLPATH --triple-threshold $THRESHOLD
