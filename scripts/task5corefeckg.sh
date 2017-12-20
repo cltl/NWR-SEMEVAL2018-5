cc38171807b80679e120b85cb61891a37479214d
piekvossen <piek.vossen@vu.nl>
2017-12-20 21:54:18 +0100

    -- first complete version that differentiates for tasks s1, s2, and s3

--- scripts/task5corefeckg.sh
+++ scripts/task5corefeckg.sh
@@ -1,14 +1,10 @@
 #!/usr/bin/env bash
 
-#Reads RDF-TRiG files and generates the ECKG and CoNLL output file for event coreference
-#Takes as parameters: path to the trig files, path to the CoNLL file and threshold for event coreference
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOT="$( cd $DIR && cd .. && pwd)"
-LIB="$ROOT"/target
-TRIGPATH=$1
-CONLLPATH=$2
-THRESHOLD=$3
+LIB="$ROOT"/lib
+TRIGPATH="/Users/piek/Desktop/SemEval2018/trial_data_final/NAFDONE"
+CONLLPATH="/Users/piek/Desktop/SemEval2018/trial_data_final/input/s3/CONLL"
+THRESHOLD="2"
 
-
-java -Xmx2000m -cp "$LIB/nwr-semeval2018-5-1.0-SNAPSHOT-jar-with-dependencies.jar" task5.Task5EventCoref -trig-files TRIGPATH --conll-files $CONLLPATH --triple-threshold $THRESHOLD
+java -Xmx2000m -cp "$LIB/nwr-semeval2018-5-1.0-SNAPSHOT-jar-with-dependencies.jar" task5.Task5EventCoref -trig-files TRIGPATH --conll-files $CONLLPATH --event-file trial_vocabulary
